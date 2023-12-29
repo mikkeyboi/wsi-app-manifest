719a09a26b6e882f66cf570c7c7a8c50753da428
Michael Leung <mw.leung93@gmail.com>
2023-12-29 13:28:34 -0700

    Update increment_version.py

--- increment_version.py
+++ increment_version.py
@@ -20,4 +20,4 @@ subprocess.run(['git', 'config', '--global', 'user.email', 'actions@github.com']
 subprocess.run(['git', 'add', 'version.txt'])
 subprocess.run(['git', 'commit', '-m', f'Increment version to {new_version}'])
 token = os.getenv('GITHUB_TOKEN')
-subprocess.run(['git', 'push', f'https://{token}@github.com/mikkeyboi/webcam_stream_incubation.git'])
+subprocess.run(['git', 'push', f'https://{token}@github.com/mikkeyboi/wsi-app-manifest.git'])
