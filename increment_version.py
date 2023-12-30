874789d35864064a6d47bccd6b3d8666e5874a6c
Michael Leung <mw.leung93@gmail.com>
2023-12-29 19:23:17 -0700

    Replaced subprocess method with bash

--- increment_version.py
+++ increment_version.py
@@ -1,5 +1,4 @@
 import os
-import subprocess
 
 # Read the current version from version.txt
 with open('version.txt', 'r') as file:
@@ -13,11 +12,3 @@ new_version = '.'.join(parts)
 # Write the new version back to version.txt
 with open('version.txt', 'w') as file:
     file.write(new_version)
-
-# Commit the changes to the repository
-subprocess.run(['git', 'config', '--global', 'user.name', 'GitHub Actions'])
-subprocess.run(['git', 'config', '--global', 'user.email', 'actions@github.com'])
-subprocess.run(['git', 'add', 'version.txt'])
-subprocess.run(['git', 'commit', '-m', f'Increment version to {new_version}'])
-token = os.getenv('GITHUB_TOKEN')
-subprocess.run(['git', 'push', f'https://{token}@github.com/mikkeyboi/wsi-app-manifest.git'])
